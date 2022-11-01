523aa848626b3a327dbfd41627723a7bd4bbb8ef
youseock <mahwin7085@gmail.com>
2022-11-01 18:01:14 +0900

    Add : nest file

---
+++ b2c-server/src/main.ts
@@ -0,0 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('API Test')
+    .setDescription('The pre-programming API document')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
